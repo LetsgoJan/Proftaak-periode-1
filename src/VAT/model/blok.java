e71d64b9d3abcdc82b77affdd23f0fa921b8a859
Maryam <maryam.mdne@gmail.com>
2019-09-10 16:49:36 +0200

    add model

--- src/VAT/model/blok.java
+++ src/VAT/model/blok.java
@@ -12,6 +12,30 @@ public class blok implements Ishape {
         this.hoogte = hoogte;
     }
 
+    public double getBreedte() {
+        return breedte;
+    }
+
+    public void setBreedte(double breedte) {
+        this.breedte = breedte;
+    }
+
+    public double getLengte() {
+        return lengte;
+    }
+
+    public void setLengte(double lengte) {
+        this.lengte = lengte;
+    }
+
+    public double getHoogte() {
+        return hoogte;
+    }
+
+    public void setHoogte(double hoogte) {
+        this.hoogte = hoogte;
+    }
+
     @Override
     public double getBlokInhoud() {
         double inhoud = this.breedte * this.lengte * this.hoogte;
