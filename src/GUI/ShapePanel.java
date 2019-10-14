a8756586018f48fdac9b227ff77830d4522f425f
jan <janvanderwant@gmail.com>
2019-10-14 21:53:41 +0200

    commit

--- src/GUI/ShapePanel.java
+++ src/GUI/ShapePanel.java
@@ -1,4 +1,19 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ShapePanel {
+    public ShapePanel(){
+
+    }
+
+    public JPanel getPanel(){
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBackground(Color.yellow);
+        panel.setBounds(10,20,800,400);
+
+        return panel;
+    }
 }
---
+++ src/model/Ishape.java
@@ -0,0 +1,13 @@
+package model;
+
+public interface Ishape {
+    public double shapeInhoud();
+    public double getStraal();
+    public double getHoogte();
+    public double getBreedte();
+    public double getLengte();
+    public void setBreedte(double breedte);
+    public void setLengte(double lengte);
+    public void setHoogte(double hoogte);
+    public void setStraal(double straal);
+}
